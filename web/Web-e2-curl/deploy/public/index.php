363d1212886dd29f53173f6739a1af7e3f9795a6
SpeciAlSerega <alexeykoko1987@mail.ru>
2020-10-04 17:19:44 +0300

    Update

--- web/Web-e2-curl/deploy/public/index.php
+++ web/Web-e2-curl/deploy/public/index.php
@@ -2,7 +2,41 @@
 
 function curl($target){
 	$result=shell_exec('curl -vvv -L -sD - -o /dev/null '.$target);
-	echo $result;
+  
+  echo '
+  
+  <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Мой секретный сервер</title>
+<style>
+body {
+background: #c7b39b url(server.jpg);
+ color: #ffff;
+}
+</style>
+</head>
+<body>
+<div class="header">
+
+    <h1 class="md-display-3" align="center">Online curl command line builder</h1>
+
+</div>
+<form>
+<div class="color_block" align="center">
+  <h1 align="center">      '.$result; echo '    </h1><br>
+
+</div>
+</form> 
+<div class="footer" align="center">
+    <p>by <a href="https://t.me/dikidikiday" target="_blank">@dikidikiday</a> </p>
+</div>
+</body>
+</html>
+  
+  
+  ';
 }
 
 if (isset($_GET['curl']) && !empty($_GET['curl']) &&!preg_match('/\s+/',$_GET['curl'])){
